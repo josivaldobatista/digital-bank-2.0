aabbf178527f560d79c9d05e2551a1411567d4b8
Josivaldo Batista <josivaldo-fb@outlook.com>
2026-03-26 13:42:53 -0300

    DIGITALBANK-03/FEATURE: Adicionado o segundo MS

---
+++ account-service/src/main/java/com/jfb/account/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfb.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
